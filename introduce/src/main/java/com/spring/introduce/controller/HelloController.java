d0fcf0979dadbc2482d1d3cb7d245b8993bd2e64
신영한 <syhan7516@naver.com>
2024-02-26 00:18:46 +0900

    API 실습 -1

--- introduce/src/main/java/com/spring/introduce/controller/HelloController.java
+++ introduce/src/main/java/com/spring/introduce/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // 컨트롤러를 의미하는 애노테이션
 @Controller
@@ -27,5 +28,16 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    // hello-string URL로 Get 요청이 들어올 경우 수행
+    @GetMapping("/hello-string")
+    
+    // Http Body에 데이터를 직접 삽입하는 애노테이션
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        
+        // ResponseBody 애노테이션 영향으로 문자를 body에 직접 반환
+        return "hello" + name;
     }
 }
